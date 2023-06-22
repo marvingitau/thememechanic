eda946c8d5cab5571fb9ae3a117967a47484fa74
marvingitau <marvingitau96@gmail.com>
2023-06-22 11:01:12 +0300

    installed the react-bootstrap-table pcks

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ThemeContext, { Context } from './context/ThemeContext';
+import ThemeContext from './context/ThemeContext';
+import Table from './pages/Table';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeContext>
-      <App />
+      <Table />
     </ThemeContext>
   </React.StrictMode>
 );
